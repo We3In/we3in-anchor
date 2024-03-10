2f76dc541c1ecd80a10b07b850d610eae7f51cb2
Bugra Okumus <saltuk.bugraokumus@gmail.com>
2024-03-10 11:08:04 +0300

    feat : write transfer_lamports

--- programs/contract/src/lib.rs
+++ programs/contract/src/lib.rs
@@ -1,6 +1,16 @@
 use anchor_lang::prelude::*;
-
+use anchor_spl::token::{self, Token, TokenAccount, Transfer as SplTransfer};
+use solana_program::system_instruction;
 declare_id!("7cBTgEf3vCgNqqp63wNAPFhXK2MDAsRz7co4kCtXJuNp");
+
+#[derive(Accounts)]
+pub struct TransferLamports<'info> {
+    #[account(mut)]
+    pub from: Signer<'info>,
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[program]
 pub mod contract {
@@ -9,7 +19,39 @@ pub mod contract {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn transfer_lamports(ctx: Context<TransferLamports>, amount: u64) -> Result<()> {
+        let from_account = &ctx.accounts.from;
+        let to_account = &ctx.accounts.to;
+
+        // Create the transfer instruction
+        let transfer_instruction =
+            system_instruction::transfer(from_account.key, to_account.key, amount);
+
+        // Invoke the transfer instruction
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                from_account.to_account_info(),
+                to_account.clone(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct TransferSpl<'info> {
+    pub from: Signer<'info>,
+    #[account(mut)]
+    pub from_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub to_ata: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+}
